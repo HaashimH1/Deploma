0851132f0a45022d03f9fcedb41931535ae89569
Haashim C <haashim379@outlook.com>
2024-05-20 13:57:04 +0000

    f- added functionality to page links, as a floating page with a smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,23 @@ document.getElementById('nav-toggle').addEventListener('click', function() {
         content.style.display = 'flex';
     }
 });
+
+document.querySelectorAll('nav a, #nav-right a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 10 * 16; // Adjust this value to set the offset
+
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - offset;
+
+        var content = document.getElementById('nav-dropdown');
+        content.style.display='none';           // removes dropdown display when link clicked on mobile
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
